b3d91a98e6a1c1d4e53e5eee892cfb5d326a450d
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2025-09-01 23:03:07 +0200

    fix auth openapi schema

--- app/lib/app_web/schemas/oauth/auth.ex
+++ app/lib/app_web/schemas/oauth/auth.ex
@@ -6,10 +6,12 @@ defmodule AppWeb.Schemas.OAuth.Auth do
       title: "Auth",
       type: :object,
       properties: %{
-        name: %Schema{type: :string, example: "John Doe"},
-        email: %Schema{type: :string, example: "john.doe@email.com"},
-        avatar_url: %Schema{type: :string, example: "https://jondoepicture.com"},
-        registration_status: %Schema{type: :string, enum: ["incomplete", "complete"], example: "incomplete"},
-      },
+        user: %Schema{type: :object, properties: %{
+          name: %Schema{type: :string, example: "John Doe"},
+          email: %Schema{type: :string, example: "john.doe@email.com"},
+          avatar_url: %Schema{type: :string, example: "https://jondoepicture.com"},
+          registration_status: %Schema{type: :string, enum: ["incomplete", "complete"], example: "incomplete"},
+        }}
+      }
     })
 end
